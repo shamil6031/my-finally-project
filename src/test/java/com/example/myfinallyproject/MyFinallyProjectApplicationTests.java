f65ce066cc11ac6e1127676d5641ae4248678f57
User <shamil.shukurov.2002@bk.ru>
2023-04-04 18:57:44 +0400

    firs commit

---
+++ src/test/java/com/example/myfinallyproject/MyFinallyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfinallyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinallyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
