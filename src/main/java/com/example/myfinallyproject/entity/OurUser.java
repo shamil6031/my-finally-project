834017369af8c28c4e66c8b9ce55e0070cafd68b
User <shamil.shukurov.2002@bk.ru>
2023-04-08 15:50:30 +0400

    My Java-Spring Project

--- src/main/java/com/example/myfinallyproject/entity/OurUser.java
+++ src/main/java/com/example/myfinallyproject/entity/OurUser.java
@@ -41,6 +41,7 @@ public class OurUser {
 
 
 
+
     public void addAccountToUser(List<AccountRequestDTO> accountRequestDTOList) {
         accountlist = new ArrayList<>();
         accountRequestDTOList.forEach(accountDTO -> accountlist.add(Account.builder()
